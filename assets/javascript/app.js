ae1372dc27a177553e5bcd80bc00c4cfc088d55f
Yvette <Yvette@macs-MacBook-Air.local>
2018-01-31 08:54:44 -0800

    Added files and firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCykgO2HeE6XxGo5KIMlu77zJGvEVQkhGc",
+    authDomain: "train-schedule-e6f0c.firebaseapp.com",
+    databaseURL: "https://train-schedule-e6f0c.firebaseio.com",
+    projectId: "train-schedule-e6f0c",
+    storageBucket: "",
+    messagingSenderId: "340342838667"
+  };
+  firebase.initializeApp(config);
+
+  
