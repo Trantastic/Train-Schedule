95f024e96d3b5b90ab67684bb1cfe5b89613f1ba
Yvette <Yvette@macs-MacBook-Air.local>
2018-01-31 20:04:46 -0800

    Added function to submit input to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,46 @@
   // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCykgO2HeE6XxGo5KIMlu77zJGvEVQkhGc",
-    authDomain: "train-schedule-e6f0c.firebaseapp.com",
-    databaseURL: "https://train-schedule-e6f0c.firebaseio.com",
-    projectId: "train-schedule-e6f0c",
-    storageBucket: "",
-    messagingSenderId: "340342838667"
-  };
-  firebase.initializeApp(config);
+var config = {
+	apiKey: "AIzaSyCykgO2HeE6XxGo5KIMlu77zJGvEVQkhGc",
+	authDomain: "train-schedule-e6f0c.firebaseapp.com",
+	databaseURL: "https://train-schedule-e6f0c.firebaseio.com",
+	projectId: "train-schedule-e6f0c",
+	storageBucket: "train-schedule-e6f0c.appspot.com",
+	messagingSenderId: "340342838667"
+};
+firebase.initializeApp(config);
 
+var database = firebase.database();
+
+// Adds trains to firebase
+$("#add-train").on("click", function(event){
+	event.preventDefault();
+
+	var trainName = $("#train-name").val().trim();
+	var trainDes = $("#destination").val().trim();
+	var trainStart = $("#first-train").val().trim();
+	var trainFreq = $("#frequency").val().trim();
+	
+	// Object to to hold values and push into firebase
+	var newTrain = {
+		name: trainName,
+		des: trainDes,
+		start: trainStart,
+		freq: trainFreq
+	};
+
+	database.ref().push(newTrain);
+
+	alert("New train successfully added");
+
+	// Clears the input boxes
+	$("#train-name").val("");
+	$("#destination").val("");
+	$("#first-train").val("");
+	$("#frequency").val("");
+});
+
+// create function to add new train info from firebase onto the html table
+
+database.ref().on("value", function(snapshot){
   
+});
